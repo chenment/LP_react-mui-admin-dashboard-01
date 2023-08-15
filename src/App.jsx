fcb26334c04ddc42c70ead722dd964e4aff3821d
chenzhengang <chenzhengang@zingfront.com>
2023-08-15 23:31:17 +0800

    Customize color token and material theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
 function App() {
-  return <div className="app"></div>
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext>
+  )
 }
 
 export default App
